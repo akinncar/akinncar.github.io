5d8aa359c0c0bec0c81a1c6719c533d05a6ea1bd
Akinn <akinncar@hotmail.com>
2020-03-05 21:27:42 -0300

    First deploy changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Akinn Rosa`,
+    description: `Personal website.`,
+    author: `@akinncar`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
